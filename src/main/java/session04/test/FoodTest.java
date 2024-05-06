1a19a323a7ec6bc3e66c205a0c18fac7b9271859
JerryWang0122 <school832@gmail.com>
2024-05-06 16:18:21 +0800

    feat: 5/6 課程結束

--- src/main/java/session04/test/FoodTest.java
+++ src/main/java/session04/test/FoodTest.java
@@ -1,12 +1,16 @@
 package session04.test;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import session04.bean.ComboMeal;
+import session04.config.FoodConfig;
 
+// 使用 java 配置在執行時要加上 VM 參數: --add-opens java.base/java.lang=ALL-UNNAMED
 public class FoodTest {
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("food-config.xml");
+        // ApplicationContext ctx = new ClassPathXmlApplicationContext("food-config.xml");  // xml配置 auto-scan
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(FoodConfig.class); // java配置 auto-scan
         ComboMeal comboMeal = ctx.getBean("comboMeal", ComboMeal.class);
         System.out.println(comboMeal.serveMeal());
     }
