d66a8c9dc1a50f9fd82b382d52493beacc323b11
JerryWang0122 <school832@gmail.com>
2024-05-06 12:02:14 +0800

    feat: scope from singleton to prototype

--- src/main/java/session01/test/TestLotto.java
+++ src/main/java/session01/test/TestLotto.java
@@ -8,7 +8,11 @@ public class TestLotto {
     public static void main(String[] args) {
         // 利用 Spring 得到 Lotto 號碼並印出
         ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config.xml");
+        // 因為 scope="prototype" 所以會實現多例而非預設的單例(singleton)
         Lotto lotto = ctx.getBean(Lotto.class);
         System.out.println(lotto.getNumbers());
+        Lotto lotto2 = ctx.getBean(Lotto.class);
+        System.out.println(lotto2.getNumbers());
+        System.out.println(lotto == lotto2);    // false
     }
 }
