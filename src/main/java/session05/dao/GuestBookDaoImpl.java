e674c78e0501999e98440b2ede019c1c9699f3c9
JerryWang0122 <school832@gmail.com>
2024-05-07 11:14:56 +0800

    feat: GuestBookDaoImpl

--- src/main/java/session05/dao/GuestBookDaoImpl.java
+++ src/main/java/session05/dao/GuestBookDaoImpl.java
@@ -1,36 +1,62 @@
 package session05.dao;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import session05.bean.GuestBook;
 
+import java.util.Date;
 import java.util.List;
 
-//@Component
-@Repository     // 他也是Component，但也同時代表他是"數據庫"資料，在意義上可以更加明確(屬於Repository的Component)
+// Repository 也是Component，但也同時代表他是"數據庫"資料
+// 在意義上可以更加明確(屬於Repository的Component)
+@Repository
 public class GuestBookDaoImpl implements GuestBookDao{
+
+    @Autowired  // 自動綁定
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public int create(GuestBook guestBook) {
-        return 0;
+        String sql = "insert into guestbook(username, content) values (?, ?)";
+        return jdbcTemplate.update(sql, guestBook.getUserName(), guestBook.getContent());
     }
 
     @Override
     public GuestBook findById(Integer id) {
-        return null;
+        // 絕對不要 select *
+        String sql = "select id, username, content, create_date, update_date from guestbook where id = ?";
+        try {
+            // queryForObject 是針對個體
+            // 搜索個體時，若找不到資料會拋異常，而不是產生null
+            GuestBook guestBook = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(GuestBook.class), id);
+            return guestBook;
+        } catch (DataAccessException e) {
+            // 訪問不到資料時
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public List<GuestBook> findAll() {
-        return null;
+        String sql = "select id, username, content, create_date, update_date from guestbook order by id";
+        // query 則是針對群組
+        // 找不到資料會給null
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(GuestBook.class));
     }
 
     @Override
     public int update(GuestBook guestBook) {
-        return 0;
+        String sql = "update guestbook set username = ?, content = ?, update_date = ? where id = ?";
+        return jdbcTemplate.update(sql, guestBook.getUserName(), guestBook.getContent(), new Date(), guestBook.getId());
     }
 
     @Override
     public int deleteById(Integer id) {
-        return 0;
+        String sql = "delete from guestbook where id = ?";
+        return jdbcTemplate.update(sql, id);
     }
 }
