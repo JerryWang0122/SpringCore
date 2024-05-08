5e54cd222f376d42bcafd950820413100ece9c71
JerryWang0122 <school832@gmail.com>
2024-05-08 11:30:57 +0800

    feat: DynProxy

--- src/main/java/session06/dyn_proxy/DynProxy.java
+++ src/main/java/session06/dyn_proxy/DynProxy.java
@@ -3,6 +3,11 @@ package session06.dyn_proxy;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 // 動態代理
 public class DynProxy {
@@ -31,6 +36,17 @@ public class DynProxy {
 
             // before
             System.out.println("寫入 log 程序...");
+            String path = "session06/dyn_proxy/log.txt";
+            String content = Arrays.toString(args);
+            try {
+                Files.write(    //寫入
+                        Paths.get(path),    // 存檔路徑
+                        content.getBytes(StandardCharsets.UTF_8),   // 轉 byte[]
+                        StandardOpenOption.CREATE,  // 若 log.txt 不存在就自動建立檔案
+                        StandardOpenOption.APPEND);     // 在 log.txt 文件末端添加資料
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
             // 調用業務方法
             result = method.invoke(object, args);
--- src/main/java/session06/dyn_proxy/TestDynProxy.java
+++ src/main/java/session06/dyn_proxy/TestDynProxy.java
@@ -7,7 +7,7 @@ import session06.proxy.Woman;
 public class TestDynProxy {
     public static void main(String[] args) {
         Calc calc = (Calc) new DynProxy(new CalcImpl()).getProxy();
-        System.out.println(calc.add(10,5));
+        System.out.println(calc.add(20,30));
         System.out.println(calc.div(10,5));
 
         Person man = (Person) new DynProxy(new Man()).getProxy();
