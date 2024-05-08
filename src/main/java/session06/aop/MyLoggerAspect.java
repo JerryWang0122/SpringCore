0ff514b0f325c9e941def318ea5876659065616c
JerryWang0122 <school832@gmail.com>
2024-05-08 14:38:23 +0800

    Refactor: AOP after

--- src/main/java/session06/aop/MyLoggerAspect.java
+++ src/main/java/session06/aop/MyLoggerAspect.java
@@ -3,6 +3,7 @@ package session06.aop;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +14,14 @@ import java.util.Arrays;
 @Order(1)   // 執行順序(數字越小越先執行)
 public class MyLoggerAspect {
 
-    @Before(value = "execution(public Integer session06.aop.ComputerImpl.add(Integer, Integer))")
+    // 切點的方法
+    @Pointcut(value = "execution(* session06.aop.ComputerImpl.*(..))")
+    public void pt1(){}
+
+    // @Before(value = "execution(public Integer session06.aop.ComputerImpl.*(Integer, Integer))")
+    // @Before(value = "execution(* session06.aop.ComputerImpl.add(..))")
+    // @Before(value = "execution(* session06.aop.ComputerImpl.*(..))")
+    @Before(value = "pt1()")
     public void beforeAdvice(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();  // 取得方法名稱
         Object[] args = joinPoint.getArgs();
