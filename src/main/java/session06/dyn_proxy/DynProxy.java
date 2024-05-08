718bc68be500bcdc556d7d6f4ea6391620ccd27a
JerryWang0122 <school832@gmail.com>
2024-05-08 11:10:28 +0800

    feat: DynProxy

--- src/main/java/session06/dyn_proxy/DynProxy.java
+++ src/main/java/session06/dyn_proxy/DynProxy.java
@@ -1,5 +1,9 @@
 package session06.dyn_proxy;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 // 動態代理
 public class DynProxy {
 
@@ -10,9 +14,27 @@ public class DynProxy {
         this.object = object;
     }
 
+    // TODO
     // 取得 Proxy 物件
     public Object getProxy() {
         Object proxyObj = null;
+
+        // 1. 載入類別器
+        ClassLoader loader = object.getClass().getClassLoader();
+
+        // 2. 被代理的物件所實作的介面
+        Class<?>[] interfaces = object.getClass().getInterfaces();
+
+        // 3. 處理代理的實現
+        InvocationHandler handler = (Object proxy, Method method, Object[] args) -> {
+            Object result = null;   // 被代理物件的業務方法的回傳值
+            result = method.invoke(object, args);   // 調用業務方法
+            return result;
+        };
+
+        // 4. 得到代理物件
+        proxyObj = Proxy.newProxyInstance(loader, interfaces, handler);
+
         return proxyObj;
     }
 }
