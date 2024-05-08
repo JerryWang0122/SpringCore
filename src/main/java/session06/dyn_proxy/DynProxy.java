e3fe9cb179f35cf2e4adc6673e89fa412dbf9168
JerryWang0122 <school832@gmail.com>
2024-05-08 12:24:45 +0800

    feat: DynProxy Aspect分離

--- src/main/java/session06/dyn_proxy/DynProxy.java
+++ src/main/java/session06/dyn_proxy/DynProxy.java
@@ -3,11 +3,6 @@ package session06.dyn_proxy;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
 
 // 動態代理
 public class DynProxy {
@@ -35,32 +30,19 @@ public class DynProxy {
             Object result = null;   // 被代理物件的業務方法的回傳值
 
             // before
-            System.out.println("寫入 log 程序...");
-            String path = "session06/dyn_proxy/log.txt";
-            String content = Arrays.toString(args);
-            try {
-                Files.write(    //寫入
-                        Paths.get(path),    // 存檔路徑
-                        content.getBytes(StandardCharsets.UTF_8),   // 轉 byte[]
-                        StandardOpenOption.CREATE,  // 若 log.txt 不存在就自動建立檔案
-                        StandardOpenOption.APPEND);     // 在 log.txt 文件末端添加資料
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            MyLoggerAspect.before(args);
 
             // 調用業務方法
             result = method.invoke(object, args);
 
             // end
-            // 示範在代理模式實現中，調整輸出結果
-            if (result != null) {
-                result = Integer.valueOf(100);
-            }
+            MyLoggerAspect.end();
 
             return result;
         };
 
         // 4. 得到代理物件
+        // 結合[類本身、實作類別、實現方法]返回代理物件
         proxyObj = Proxy.newProxyInstance(loader, interfaces, handler);
 
         return proxyObj;
