bbcde59679efb30d76382b51f55814589f16c05a
JerryWang0122 <school832@gmail.com>
2024-05-08 10:17:39 +0800

    feat: after proxy

--- src/main/java/session06/test/TestPerson.java
+++ src/main/java/session06/test/TestPerson.java
@@ -2,14 +2,15 @@ package session06.test;
 
 import session06.proxy.Man;
 import session06.proxy.Person;
+import session06.proxy.PersonProxy;
 import session06.proxy.Woman;
 
 public class TestPerson {
     public static void main(String[] args) {
-        Person man = new Man();
-        man.work();
+        Person man = new PersonProxy(new Man());
+        Person woman = new PersonProxy(new Woman());
 
-        Person woman = new Woman();
+        man.work();
         woman.work();
     }
 }
