b1cbfe9573f537b61bc9c2e1e7d3433b992c9860
JerryWang0122 <school832@gmail.com>
2024-05-08 10:15:20 +0800

    feat: before proxy

--- src/main/java/session06/proxy/Man.java
+++ src/main/java/session06/proxy/Man.java
@@ -3,7 +3,11 @@ package session06.proxy;
 public class Man implements Person{
     @Override
     public void work() {
+        // 公用邏輯
+        System.out.println("出門戴口罩");
         // 業務邏輯
         System.out.println("上班工作");
+        // 公用邏輯
+        System.out.println("回家要洗手");
     }
 }
--- src/main/java/session06/proxy/Woman.java
+++ src/main/java/session06/proxy/Woman.java
@@ -4,7 +4,11 @@ public class Woman implements Person {
 
     @Override
     public void work() {
+        // 公用邏輯
+        System.out.println("出門戴口罩");
         // 業務邏輯
         System.out.println("市場買菜");
+        // 公用邏輯
+        System.out.println("回家要洗手");
     }
 }
